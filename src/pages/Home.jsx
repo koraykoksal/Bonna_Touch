ca0d51cbc4ee81578b7605e6363dd7db2115500b
koraykoksal <kksl.koray@gmail.com>
2023-08-08 11:18:18 +0300

    routes and pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Headers } from '../components/Headers'
+
+export const Home = () => {
+  return (
+
+    <div>
+        <Headers/>
+    </div>
+
+  )
+}
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Headers } from '../components/Headers'
+import { NavBar } from '../components/NavBar'
+import { Route, Routes } from 'react-router-dom'
+import { Login } from '../pages/Login'
+import { Register } from '../pages/Register'
+import { Home } from '../pages/Home'
 
 export const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    
+    <>
+    <NavBar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+    </Routes>
+    </>
   )
 }
