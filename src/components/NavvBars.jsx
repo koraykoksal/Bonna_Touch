422aac642048e304f8e4b97def1239f592f0620a
koraykoksal <kksl.koray@gmail.com>
2023-08-14 01:25:09 +0300

    navbars changed

--- src/components/NavvBars.jsx
+++ src/components/NavvBars.jsx
@@ -1,7 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import bonnaLogo from '../assets/img/bonnaTouchLogo.png'
 
 
@@ -18,10 +18,11 @@ function classNames(...classes) {
 
 export default function NavvBars() {
 
+    const navigate=useNavigate()
 
   return (
 
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-gray-400">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -38,15 +39,24 @@ export default function NavvBars() {
                   )}
                 </Disclosure.Button>
               </div>
+              
+              
+
+                <div className="flex flex-shrink-0 items-center">
+                <img
+                    className="h-16 w-auto hover:cursor-pointer"
+                    src={bonnaLogo}
+                    alt="Your Company"
+                    onClick={()=>window.open('https://bonna.com.tr')}
+                />
+                  
+                </div>
+
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 
-                <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
-                </div>
+                
+
+                
 
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
@@ -64,18 +74,12 @@ export default function NavvBars() {
                 </div>
 
               </div>
+
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
 
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
+
                   <div>
                     <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-1.5" />
@@ -87,6 +91,7 @@ export default function NavvBars() {
                       />
                     </Menu.Button>
                   </div>
+
                   <Transition
                     as={Fragment}
                     enter="transition ease-out duration-100"
@@ -130,24 +135,26 @@ export default function NavvBars() {
                     </Menu.Items>
                   </Transition>
                 </Menu>
+
               </div>
+
             </div>
           </div>
 
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item,i) => (
-                // <Disclosure.Button 
-                //   key={i}
-                //   as="a"
+                <Disclosure.Button 
+                  key={i}
+                  onClick={()=>navigate(item.url)}
                 //   to={item.url}
-                //   className='focus:bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium hover:cursor-pointer'
-                // >
-                //   {item.name}
-                // </Disclosure.Button>
-                <Link key={i} to={item.url} className='focus:bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium hover:cursor-pointer'>
-                    {item.name}
-                    </Link>
+                  className='focus:bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium hover:cursor-pointer'
+                >
+                  {item.name}
+                </Disclosure.Button>
+                // <Link key={i} to={item.url} className='focus:bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium hover:cursor-pointer'>
+                //     {item.name}
+                //     </Link>
               ))}
             </div>
           </Disclosure.Panel>
