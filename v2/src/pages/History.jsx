1c4567819fa6000ec8d770fe5a6d11fcabc15ca1
koraykoksal <kksl.koray@gmail.com>
2023-08-16 14:54:01 +0300

    history filtering and mapping

--- v2/src/pages/History.jsx
+++ v2/src/pages/History.jsx
@@ -10,20 +10,15 @@ export default function History() {
 
   const {dalleData} = useSelector((state)=>state.touch)
 
-  let currentTime = new Date().toLocaleTimeString()
-  
   let d = new Date()
-  let h = new Date().getHours()
-  d.setHours(h+2)
-
-  console.log(d.toLocaleTimeString())
+  let currentime = d.toLocaleTimeString()
 
 
   return (
 
     <div className='flex flex-wrap justify-center items-center gap-5 my-12'>
 
-      {dalleData.map((data) =>(
+      {/* {dalleData.map((data) =>(
 
         <Card sx={{ maxWidth: 345 }} key={data.id}>
         <CardActionArea>
@@ -37,12 +32,36 @@ export default function History() {
             <Typography variant="body2" color="text.secondary">
               {data.prompt}
             </Typography>
-            <Typography>
+            <Typography variant="body2" color="text.secondary">
             Expiry:{data.endtime}
             </Typography>
           </CardContent>
         </CardActionArea>
         </Card>
+      ))} */}
+
+      {dalleData.filter((item)=>currentime <= item.endtime).map((data)=>(
+
+        <Card sx={{ maxWidth: 345 }} key={data.id}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={data.promptImg}
+            
+          />
+          <CardContent sx={{display:'flex',alignItems:'center',justifyContent:'space-between'}}>
+            <Typography variant="body2" color="text.secondary">
+              {data.prompt}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+            Expiry:{data.endtime}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        </Card>
+
+
       ))}
 
 
