74b0dcedf04f757baece0f9993544c0b3c685adf
koraykoksal <kksl.koray@gmail.com>
2024-01-17 12:24:49 +0300

    notfound page

--- v4/src/pages/NotFound.jsx
+++ v4/src/pages/NotFound.jsx
@@ -1,31 +1,38 @@
 import React from 'react'
 import { useNavigate } from 'react-router'
+import Typography from '@mui/material/Typography';
+import { Box, CardActionArea, Container } from '@mui/material';
+import notFound from "../assets/img/notfound.png"
+import { RiHomeLine } from "react-icons/ri";
+import { IoReturnDownBack } from "react-icons/io5";
+
+
 
 export const NotFound = () => {
 
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 
   return (
-   
-    <>
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <p className=" font-semibold text-indigo-600 text-7xl">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
-        <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <button
-            href="#"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            onClick={()=>navigate('/')}
-          >
-            Go back home
-          </button>
 
-        </div>
-      </div>
-    </main>
-  </>
+    <>
+
+      <Box display={'flex'} flexDirection={'column'} minHeight={'1100px'} gap={5} alignItems={'center'}>
+
+        <Container sx={{display:'flex',justifyContent:'center',p:5,mt:10}}>
+        <img
+          src={notFound}
+          height={'500px'}
+          style={{objectFit:'contain'}}
+        />
+        </Container>
+
+        <Container sx={{display:'flex',justifyContent:'space-evenly',gap:5,p:5}}>
+          <RiHomeLine size={50} cursor={'pointer'} onClick={()=>navigate('/home')}/>
+          <IoReturnDownBack size={50} cursor={'pointer'} onClick={()=>navigate(-1)}/>
+        </Container>
+
+      </Box>
+    </>
 
   )
 }
